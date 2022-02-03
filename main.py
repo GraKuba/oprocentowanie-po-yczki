27909b454ae3d8790cd1aaae895444c923574b2c
Jakub <grabarczyk.kuba@gmail.com>
2022-02-03 18:17:32 +0100

    Dodanie oprocentowania inflacji i 'Zadluzenie='

--- main.py
+++ main.py
@@ -1,99 +1,101 @@
 initial_loan = 12000
 fixed_instalment = 200
+intrest_rate = 3
 
 inflation = float(input())
-debt = (1 + (inflation + 3) / 1200) * initial_loan - fixed_instalment
-print(debt)
+debt = (1 + (inflation + intrest_rate) / 1200) * initial_loan - fixed_instalment
+print("Zadluzenie =", debt)
 
 inflation = float(input())
-debt = (1 + (inflation + 3) / 1200) * debt - fixed_instalment
-print(debt)
+debt = (1 + (inflation + intrest_rate) / 1200) * debt - fixed_instalment
+print("Zadluzenie =", debt)
 
 inflation = float(input())
-debt = (1 + (inflation + 3) / 1200) * debt - fixed_instalment
-print(debt)
+debt = (1 + (inflation + intrest_rate) / 1200) * debt - fixed_instalment
+print("Zadluzenie =", debt)
 
 inflation = float(input())
-debt = (1 + (inflation + 3) / 1200) * debt - fixed_instalment
-print(debt)
+debt = (1 + (inflation + intrest_rate) / 1200) * debt - fixed_instalment
+print("Zadluzenie =", debt)
 
 inflation = float(input())
-debt = (1 + (inflation + 3) / 1200) * debt - fixed_instalment
-print(debt)
+debt = (1 + (inflation + intrest_rate) / 1200) * debt - fixed_instalment
+print("Zadluzenie =", debt)
 
 inflation = float(input())
-debt = (1 + (inflation + 3) / 1200) * debt - fixed_instalment
-print(debt)
+debt = (1 + (inflation + intrest_rate) / 1200) * debt - fixed_instalment
+print("Zadluzenie =", debt)
 
 inflation = float(input())
-debt = (1 + (inflation + 3) / 1200) * debt - fixed_instalment
-print(debt)
+debt = (1 + (inflation + intrest_rate) / 1200) * debt - fixed_instalment
+print("Zadluzenie =", debt)
 
 inflation = float(input())
-debt = (1 + (inflation + 3) / 1200) * debt - fixed_instalment
-print(debt)
+debt = (1 + (inflation + intrest_rate) / 1200) * debt - fixed_instalment
+print("Zadluzenie =", debt)
 
 inflation = float(input())
-debt = (1 + (inflation + 3) / 1200) * debt - fixed_instalment
-print(debt)
+debt = (1 + (inflation + intrest_rate) / 1200) * debt - fixed_instalment
+print("Zadluzenie =", debt)
 
 inflation = float(input())
-debt = (1 + (inflation + 3) / 1200) * debt - fixed_instalment
-print(debt)
+debt = (1 + (inflation + intrest_rate) / 1200) * debt - fixed_instalment
+print("Zadluzenie =", debt)
 
 inflation = float(input())
-debt = (1 + (inflation + 3) / 1200) * debt - fixed_instalment
-print(debt)
+debt = (1 + (inflation + intrest_rate) / 1200) * debt - fixed_instalment
+print("Zadluzenie =", debt)
 
 inflation = float(input())
-debt = (1 + (inflation + 3) / 1200) * debt - fixed_instalment
-print(debt)
+debt = (1 + (inflation + intrest_rate) / 1200) * debt - fixed_instalment
+print("Zadluzenie =", debt)
 
 inflation = float(input())
-debt = (1 + (inflation + 3) / 1200) * debt - fixed_instalment
-print(debt)
+debt = (1 + (inflation + intrest_rate) / 1200) * debt - fixed_instalment
+print("Zadluzenie =", debt)
 
 inflation = float(input())
-debt = (1 + (inflation + 3) / 1200) * debt - fixed_instalment
-print(debt)
+debt = (1 + (inflation + intrest_rate) / 1200) * debt - fixed_instalment
+print("Zadluzenie =", debt)
 
 inflation = float(input())
-debt = (1 + (inflation + 3) / 1200) * debt - fixed_instalment
-print(debt)
+debt = (1 + (inflation + intrest_rate) / 1200) * debt - fixed_instalment
+print("Zadluzenie =", debt)
 
 inflation = float(input())
-debt = (1 + (inflation + 3) / 1200) * debt - fixed_instalment
-print(debt)
+debt = (1 + (inflation + intrest_rate) / 1200) * debt - fixed_instalment
+print("Zadluzenie =", debt)
 
 inflation = float(input())
-debt = (1 + (inflation + 3) / 1200) * debt - fixed_instalment
-print(debt)
+debt = (1 + (inflation + intrest_rate) / 1200) * debt - fixed_instalment
+print("Zadluzenie =", debt)
 
 inflation = float(input())
-debt = (1 + (inflation + 3) / 1200) * debt - fixed_instalment
-print(debt)
+debt = (1 + (inflation + intrest_rate) / 1200) * debt - fixed_instalment
+print("Zadluzenie =", debt)
 
 inflation = float(input())
-debt = (1 + (inflation + 3) / 1200) * debt - fixed_instalment
-print(debt)
+debt = (1 + (inflation + intrest_rate) / 1200) * debt - fixed_instalment
+print("Zadluzenie =", debt)
 
 inflation = float(input())
-debt = (1 + (inflation + 3) / 1200) * debt - fixed_instalment
-print(debt)
+debt = (1 + (inflation + intrest_rate) / 1200) * debt - fixed_instalment
+print("Zadluzenie =", debt)
 
 inflation = float(input())
-debt = (1 + (inflation + 3) / 1200) * debt - fixed_instalment
-print(debt)
+debt = (1 + (inflation + intrest_rate) / 1200) * debt - fixed_instalment
+print("Zadluzenie =", debt)
 
 inflation = float(input())
-debt = (1 + (inflation + 3) / 1200) * debt - fixed_instalment
-print(debt)
+debt = (1 + (inflation + intrest_rate) / 1200) * debt - fixed_instalment
+print("Zadluzenie =", debt)
 
 inflation = float(input())
-debt = (1 + (inflation + 3) / 1200) * debt - fixed_instalment
-print(debt)
+debt = (1 + (inflation + intrest_rate) / 1200) * debt - fixed_instalment
+print("Zadluzenie =", debt)
 
 inflation = float(input())
-debt = (1 + (inflation + 3) / 1200) * debt - fixed_instalment
-print(debt)
+debt = (1 + (inflation + intrest_rate) / 1200) * debt - fixed_instalment
+print("Zadluzenie =", debt)
+
 
