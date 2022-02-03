5961995928a78b97fe211d702308b0125ba8d401
Jakub <grabarczyk.kuba@gmail.com>
2022-02-03 18:02:22 +0100

    Poprawa wzoru na obliczenie zadluzenia

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ initial_loan = 12000
 fixed_instalment = 200
 
 inflation = float(input())
-
-debt = (1 + (inflation + 3) / 1200) * 12000 - 200
-
+debt = (1 + (inflation + 3) / 1200) * initial_loan - fixed_instalment
 print(debt)
+
 
 
 
