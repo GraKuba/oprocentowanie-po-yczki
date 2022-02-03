340820a33ec630d85fe928ded1328c39c49218d1
Jakub <grabarczyk.kuba@gmail.com>
2022-02-03 18:49:44 +0100

    Dodanie zaokraglenia wartosci

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ initial_loan = 12000
 fixed_instalment = 200
 intrest_rate = 3
 
-tpl_message = message ="Twoja pozostala kwota kredytu to {}. To o X mniej niz w zeszlym miesiacu."
+tpl_message = message ="Twoja pozostala kwota kredytu to {}. To o {} mniej niz w zeszlym miesiacu."
 
 inflation = float(input())
 debt = (1 + (inflation + intrest_rate) / 1200) * initial_loan - fixed_instalment
-message = tpl_message.format(debt)
+message = tpl_message.format(round(debt, 2), round(initial_loan - debt, 2))
 print(message)
+
+debt_change = initial_loan - debt
 
 inflation = float(input())
 debt = (1 + (inflation + intrest_rate) / 1200) * debt - fixed_instalment
-message = tpl_message.format(debt)
+message = tpl_message.format(round(debt, 2), round(debt_change, 2))
 print(message)
 
 
