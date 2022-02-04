890644f69c7f5470ad0897251eb4e87a2467756a
Jakub <grabarczyk.kuba@gmail.com>
2022-02-04 10:45:16 +0100

    Poprawienie kodu przez skasowanie pierwszej czesci i zmiany 'initial debt' na 'debt'

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-initial_loan = 12000
+debt = 12000
 fixed_instalment = 200
 interest_rate = 3
 month_number = 1
 
 tpl_message ="Twoja pozostala kwota kredytu to {}. To o {} mniej niz w zeszlym miesiacu."
-
-inflation = float(input())
-debt = (1 + (inflation + interest_rate) / 1200) * initial_loan - fixed_instalment
-message = tpl_message.format(round(debt, 2), round(initial_loan - debt, 2))
-print("Month", month_number, message)
-month_number += 1
 
 while month_number <= 24:
     initial_debt = debt
